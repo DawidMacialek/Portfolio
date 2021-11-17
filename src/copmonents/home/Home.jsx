09d8cb500aee10f02f5961bde0667f3fd30dc19b
Dawid Macialek <macialekdawid@gmail.com>
2021-11-17 21:33:25 +0100

    home page, typing quote and navigation arrow

--- src/copmonents/home/Home.jsx
+++ src/copmonents/home/Home.jsx
@@ -1,9 +1,58 @@
-import "./home.scss";
+import { useEffect, useRef } from 'react';
+import { init } from 'ityped';
+import './home.scss';
+import 'material-icons/iconfont/material-icons.css';
+import { quote } from '../../data/homePage';
+import { ABOUT } from '../../data/menuElements';
 
-function Home() {
+function Home({}) {
+  // const [letterQuote, setLetterQuote] = useState([]);
+  const refToQuote = useRef();
+  const refToAuthor = useRef();
+  // quote typing
+  useEffect(() => {
+    const isTypping = 150;
+    init(refToQuote.current, {
+      showCursor: false,
+      strings: [quote[0]],
+      typeSpeed: isTypping,
+      loop: false,
+      cursorChar: '|',
+      onFinished: function () {
+        init(refToAuthor.current, {
+          showCursor: false,
+          strings: [quote[1]],
+          typeSpeed: 50,
+          loop: false,
+          cursorChar: '|',
+          startDelay: 1000,
+        });
+      },
+    });
+  }, []);
+
   return (
-    <div className="welcome" id="home">
-      <h1>Welcome</h1>
+    <div className='home-container' id='home'>
+      <div className='home'>
+        <div className='quote-container'>
+          <span className='quote' ref={refToQuote}></span>
+          <span className='author' ref={refToAuthor}></span>
+        </div>
+        <div className='welcome'>
+          <h1>
+            Jestem Dawid Macialek,
+            <br />
+            Witam Cię na moim portfolio!
+          </h1>
+        </div>
+        <div className='arrow-container'>
+          <a href={`#${ABOUT}`}>
+            <span class='material-icons-round arrow'>
+              keyboard_double_arrow_down
+            </span>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data/homePage.js
@@ -0,0 +1,5 @@
+// quote store remebmer structure name = ["quote", "author"]
+export const quote = [
+  'Nawet najdalszą podróż zaczyna się od pierwszego kroku.',
+  'Lao-tzu',
+];
