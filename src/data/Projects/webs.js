2eff7ee06b12608a700fd0ea55ac980f669fb241
Dawid Macialek <macialekdawid@gmail.com>
2022-02-23 12:59:33 +0100

    adding another project quiz app

--- src/data/Projects/webs.js
+++ src/data/Projects/webs.js
@@ -1,5 +1,7 @@
 import miniWeatherApp from '../assets/weather-app-mini.png';
 import weatherApp from '../assets/weather-app.png';
+import miniQuizApp from '../assets/quiz-app-mini.png';
+import quizApp from '../assets/quiz-app-big.png';
 
 export const webs = [
   {
@@ -11,5 +13,15 @@ export const webs = [
     Picture: weatherApp,
     gitHubHref: 'https://github.com/DawidMacialek/WeatherAPP',
     playHref: 'https://dawidmacialek.github.io/WeatherAPP/',
+  },
+  {
+    id: 1,
+    name: 'QuizApp',
+    description:
+      'QuizApp to aplikacja webowa polegająca na rozwiązywaniu quizu. Uzytkownik wybiera kategorię oraz ilość pytań na które chce odpowiadać. Ma zaznaczyć prawidłową odpowiedź, jedną z czterech odpowiedzi. Na każdą odpowiedź użytkownik ma 1 minutę. Kiedy czas się skończy, automatycznie zostanie wyświetlony wynik. Do generowania pytań aplikacja korzysta z zewnętrzenego API https://opentdb.com/.  ',
+    miniPicture: miniQuizApp,
+    Picture: quizApp,
+    gitHubHref: 'https://github.com/DawidMacialek/quizApp',
+    playHref: 'https://dawidmacialek.github.io/quizApp/',
   },
 ];
